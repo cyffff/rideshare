0969ffaf29863dd7080d6ee15e6e22f1351c09cd
cyf <yunfei.cao@payby.com>
2025-03-15 13:31:13 +0400

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import BookRide from './pages/BookRide';
-import RideHistory from './pages/RideHistory';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
-    },
-    secondary: {
-      main: '#dc004e',
-    },
-  },
-});
+import './App.css';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/book-ride" element={<BookRide />} />
-          <Route path="/ride-history" element={<RideHistory />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <h1>Ride Sharing Application</h1>
+        <p>Welcome to our ride-sharing platform!</p>
+      </header>
+    </div>
   );
 }
 
